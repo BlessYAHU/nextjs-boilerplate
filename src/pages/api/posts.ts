e091b6599fdcdfa0d765857226ff727eafedb154
A'braham Barakhyahu <barakhyahu@gmail.com>
2021-06-19 15:28:15 -0400

    implements code to put and get items to leveldb.

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,3 +1,4 @@
+import * as level from 'level';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { fetchPosts, Post } from '../../services/postsService';
@@ -7,7 +8,30 @@ interface Response {
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse<Response>) => {
-  const posts = await fetchPosts();
+  console.log('open');
+  // 1) Create our database, supply location and options.
+  //    This will create or open the underlying store.
+  const db = level('my-db');
+  try {
+    let val;
+    db.get('posts', (err, value) => {
+      if (err) throw new Error('Ooops! ' + err); // likely the key was not found
+      val = value;
+      // Ta da!
+      console.log('name=' + value);
+    });
+    if (val) {
+      console.log('value: ' + val);
+    } else {
+      val = await fetchPosts();
+      await db.put('posts', JSON.stringify(val));
+    }
 
-  res.status(200).json(posts);
+    res.status(200).json(val); //.json();
+  } catch (e) {
+    // console.log('error ' + JSON.stringify(e));
+    res.status(404).send('error'); //.send(value);//.json();
+  } finally {
+    db.close();
+  }
 };
